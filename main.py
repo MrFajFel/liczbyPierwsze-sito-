d22c76e4d057a457a35456ba58ae97c4d28d553e
Kinga Smolarek <100038018+Radio2840@users.noreply.github.com>
2025-05-16 12:19:51 +0200

    zrobilo sie

--- main.py
+++ main.py
@@ -2,4 +2,23 @@ class Sito:
 
     def __init__(self,n):
         self.n = n
-        self.pierwsze = [True for i in range(n)]
+        self.pierwsze = [True for i in range(n+1)]
+
+    def generuj(self):
+        for i in range(2,int(self.n ** 0.5)+1):
+            if self.pierwsze[i]:
+                w = i*2
+                while w <= self.n:
+                    self.pierwsze[w] = False
+                    w += i
+
+    def wyswietl(self):
+        for i in range(2,self.n+1):
+            if self.pierwsze[i]:
+                print(i, end=" ")
+
+
+
+sito = Sito(10000)
+sito.generuj()
+sito.wyswietl()
